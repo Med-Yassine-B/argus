fe18840778ec2de87f3c782df2e25a0b13e07a1b
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-03-26 14:35:01 +0100

    Refactored calc file

--- calc.py
+++ calc.py
@@ -1,26 +1,3 @@
-def checkOp(num1,num2,op):
-    # Tipp: Auf Ifelse verzichten, wenn davon mehr als 3 Stück entstehen
-    match op:
-        case '+':
-            return num1 + num2
-        case '-': 
-            return num1 - num2
-        case '*': 
-            return num1 * num2
-        case '/': 
-            try:
-                return num1 / num2
-            except ZeroDivisionError:
-                print("Divison durch null nicht möglich!")
-                return None
-        case '%': 
-            return num1 % num2
-        case '**': 
-            return num1 ** num2
-        case _:
-            # Tipp: Für undefinierte Zustände einfach 'None' zurückgeben
-            return None
-        
 def getNum():
     # Tipp: Bool-Werte müssen nicht in Klammern stehen
     while True:
@@ -29,6 +6,34 @@ def getNum():
             return num 
         except ValueError:
             print("Bitte eine Zahl eingeben!!")
+
+def getOperator():
+    while True:
+        op = input("Welche Rechenoperation wollen Sie anwenden? (+,-,*,/,%,**) ")
+        if (op == '+' or '-' or '*' or '/' or '%' or '**'):
+            return op
+        else:
+            print("Bitte erneut eingeben!")
+
+def calc(num1,num2,op):
+        # Tipp: Auf Ifelse verzichten, wenn davon mehr als 3 Stück entstehen
+        match op:
+            case '+':
+                return num1 + num2
+            case '-': 
+                return num1 - num2
+            case '*': 
+                return num1 * num2
+            case '/': 
+                try:
+                    return num1 / num2
+                except ZeroDivisionError:
+                    print("Divison durch null nicht möglich!")
+                    return None
+            case '%': 
+                return num1 % num2
+            case '**': 
+                return num1 ** num2
     
 def main():
     initConv = "Willkommen zum Basic Calculator!"
@@ -39,16 +44,8 @@ def main():
         # Tipp: Man muss nicht die Variable weiterreichen, die zum Speichern des Return-Values da ist
         num1 = getNum()
         num2 = getNum()
-
-        while(True):
-            op = input("Welche Rechenoperation wollen Sie anwenden? (+,-,*,/,%,**) ")
-            result = checkOp(num1,num2,op)
-
-            if result is None:
-                print("Bitte erneut eingeben!")
-            else:
-                break
-
+        op = getOperator()
+        result = calc(num1,num2,op)
         print(result)
         while True:
             repeat = input("Wollen Sie eine neue Berechnung ausführen? (y/n) ")
