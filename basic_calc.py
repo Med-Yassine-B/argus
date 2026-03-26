63ae8b72cb104c45f1873fded32fe348b030075f
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-03-26 11:29:57 +0100

    Add basic_calc.py

---
+++ basic_calc.py
@@ -0,0 +1,12 @@
+
+
+x = 5
+y = 5
+
+
+print("Addition:        ",x + y) 
+print("Subtaktion:      ",x - y) 
+print("Multiplikation:  ",x * y) 
+print("Division:        ",x / y)     
+print("Potenz:          ",x ** y)
+print("Modulo:          ",x % y)
