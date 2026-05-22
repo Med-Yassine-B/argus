b12a41c1d4198fd01365569c3cc726d696036fe7
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-05-22 18:12:34 +0200

    feat(#1): add test for client and ignore temp folder

--- temp/basic_input.py
+++
@@ -1,5 +0,0 @@
-name = input("Wie heißt du? ") # Inpu() gibt nur String zurück -> Eingaben zum Rechnen gehen nicht einfach so
-print("Hallo",name)
-
-zahl = int(input("Gib eine Zahl ein, die mit 5 addiert wird: "))
-print("Ergebnis",zahl + 5)
