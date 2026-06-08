1d3c74c7b4f3d41ca7395e687abb249c7431c6ec
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-08 16:01:59 +0200

    feat(#5): add all basic metrics

--- src/fx_converter_lab/analytics/metrics/trend_metrics.py
+++ src/fx_converter_lab/analytics/metrics/trend_metrics.py
@@ -1,7 +1,23 @@
 import pandas as pd
 from fx_converter_lab.services.timeseries_service import create_timeseries
 
-def add_daily_percentage_change(curr: str, df: pd.DataFrame) -> pd.DataFrame:
-    df = create_timeseries(curr, df)
+def add_daily_percentage_change(df: pd.DataFrame) -> pd.DataFrame:
     df["d_change_rate"] = df["rate"].pct_change() * 100
     return df
+
+def add_rolling_average(df: pd.DataFrame) -> pd.DataFrame:
+    df["roll_avg"] = df["rate"].rolling(window=3).mean()
+    return df
+
+def get_min_max_rates(df: pd.DataFrame) -> dict:
+    min_max = {
+        "rate":[],
+        "date":[]
+    }
+    min_id = df["rate"].idxmax()
+    max_id = df["rate"].idxmin()
+    min_max["rate"].append(df.loc[min_id, "rate"])
+    min_max["date"].append(df.loc[min_id, "date"])
+    min_max["rate"].append(df.loc[max_id, "rate"])
+    min_max["date"].append(df.loc[max_id, "date"])
+    return min_max
