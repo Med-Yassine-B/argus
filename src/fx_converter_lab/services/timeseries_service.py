7ae3804ca5050f8a9a8ea5c4554a2feb0b367a73
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-08 16:58:12 +0200

    refactor(#5): remove one function

--- src/fx_converter_lab/services/timeseries_service.py
+++ src/fx_converter_lab/services/timeseries_service.py
@@ -1,22 +1,11 @@
 import pandas as pd
 from fx_converter_lab.clients.mock_client import get_mock_timeseries
-from fx_converter_lab.domain.validation import normalize_input_string,is_valid_curr_code
-
-def check_currency(question:str) -> str:
-    resp = normalize_input_string(question)
-    """
-    if is_valid_curr_code(resp):
-        return resp
-    return None
-    """
-    return resp
 
 def create_timeseries(curr: str, dates: pd.DataFrame) -> pd.DataFrame:
-    resp = check_currency(curr)
     dates["rate"] = 0.0
     for i in range(len(dates)):
         date = str(dates.loc[i,"date"])
-        dates.loc[i, "rate"] = get_mock_timeseries(resp, date)
+        dates.loc[i, "rate"] = get_mock_timeseries(curr, date)
     return dates
 
 
