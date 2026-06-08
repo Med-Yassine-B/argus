017ef66bafbf807b9ba738ad744d859b129157a1
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-08 16:51:18 +0200

    feat(#5): add min and max rates to chart

--- src/fx_converter_lab/analytics/metrics/trend_metrics.py
+++ src/fx_converter_lab/analytics/metrics/trend_metrics.py
@@ -11,13 +11,15 @@ def add_rolling_average(df: pd.DataFrame) -> pd.DataFrame:
 
 def get_min_max_rates(df: pd.DataFrame) -> dict:
     min_max = {
-        "rate":[],
-        "date":[]
+        "min_date":[],
+        "min_rate":[],
+        "max_date":[],
+        "max_rate":[]
     }
-    min_id = df["rate"].idxmax()
-    max_id = df["rate"].idxmin()
-    min_max["rate"].append(df.loc[min_id, "rate"])
-    min_max["date"].append(df.loc[min_id, "date"])
-    min_max["rate"].append(df.loc[max_id, "rate"])
-    min_max["date"].append(df.loc[max_id, "date"])
+    min_id = df["rate"].idxmin()
+    max_id = df["rate"].idxmax()
+    min_max["min_date"].append(df.loc[min_id, "date"])
+    min_max["min_rate"].append(df.loc[min_id, "rate"])
+    min_max["max_date"].append(df.loc[max_id, "date"])
+    min_max["max_rate"].append(df.loc[max_id, "rate"])
     return min_max
