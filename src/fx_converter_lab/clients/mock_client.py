335484a3e79ab9387459a29b337cf67e480a64a8
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-08 14:25:53 +0200

    feat(#5): create trendchart with mock data

--- src/fx_converter_lab/clients/mock_client.py
+++ src/fx_converter_lab/clients/mock_client.py
@@ -1,20 +1,25 @@
 import pandas as pd
 
-data = {
+mock_resp_data = {
     "date": ["2026-06-01","2026-06-02","2026-06-03",
              "2026-06-04","2026-06-05","2026-06-06",
              "2026-06-07","2026-06-08","2026-06-09",
              "2026-06-10","2026-06-11","2026-06-12",
              "2026-06-13","2026-06-14","2026-06-15"
+
              ],
     "rate": [1.08,1.10,1.14,
              1.12,1.10,1.07,
              1.08,1.08,1.08,
-             1.10,1.10,1,13,
-             1.15,1.13,1.12
-            ]
+             1.10,1.12,1.12,
+             1.15,1.12,1.12
+            ],
 }
 
-def get_mock_timeseries():
-    df = pd.DataFrame(data)
-    return df
+def get_mock_timeseries(curr: str, date: str) -> int | None:
+    if(curr=="USD"):
+        index = mock_resp_data["date"].index(date)
+        rate = mock_resp_data["rate"][index]
+        return rate 
+    else:
+        return None
