f3934bfd2ec8133e34bc964d2fa2349b777f9c2b
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-09 10:42:10 +0200

    test(#5): add tests for trend metrics

--- tests/test_validation_domain.py
+++ tests/test_validation_domain.py
@@ -3,37 +3,37 @@ import requests as req
 from unittest.mock import Mock
 from fx_converter_lab.domain.validation import *
 
-def test_op_is_valid(monkeypatch):
+def test_op_is_valid():
     data = is_valid_op('+')
 
     assert data is True
 
-def test_op_is_not_valid(monkeypatch):
+def test_op_is_not_valid():
     data = is_valid_op('LOL')
 
     assert data is False
 
-def test_curr_is_valid(monkeypatch):
+def test_curr_is_valid():
     data = is_valid_curr_code('AOA')
 
     assert data is True
 
-def test_curr_is_not_valid(monkeypatch):
+def test_curr_is_not_valid():
     data = is_valid_curr_code('LOL')
 
     assert data is False
 
-def test_parse_amount_valid(monkeypatch):
+def test_parse_amount_valid():
     data = parse_amount('20.2')
 
     assert data == 20.2
 
-def test_parse_amount_not_valid(monkeypatch):
+def test_parse_amount_not_valid():
     data = parse_amount('fuck')
 
     assert data is None
 
-def test_normalizing_string(monkeypatch):
+def test_normalizing_string():
     data = normalize_input_string(' lOl ')
 
     assert data == 'LOL'
