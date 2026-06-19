1943f5a6c92d0f8a041b5731058f041693c4c064
Lev Gusiev <lev.gusiev@stud.htwk-leipzig.de>
2026-06-20 01:33:12 +0200

    feat(#33): remove debug prints

--- src/argus/analytics/charts/trend_chart.py
+++ src/argus/analytics/charts/trend_chart.py
@@ -31,8 +31,10 @@ def create_trendchart(curr_symbol: str, start:str,end:str,interval:str):
         Minimum and maximum exchange-rate values are marked with scatter
         points and annotations.
     """
-
-    df, min_max_rates = prepare_trend_analysis(curr_symbol,start,end,interval)
+    result = prepare_trend_analysis(curr_symbol,start,end,interval)
+    if result is None:
+        return None
+    df, min_max_rates = result
     min_date = min_max_rates["min_date"][0]
     min_rate = min_max_rates["min_rate"][0]
     max_date = min_max_rates["max_date"][0]
--- src/argus/clients/yfinancy_client.py
+++ src/argus/clients/yfinancy_client.py
@@ -9,9 +9,6 @@ def get_timeseries(curr_symbol,start,end,interval):
     data = data.reset_index()
     data = data[["Date","Close"]]
     data = data.rename(columns={"Date": "date", "Close": "rate"})
-    print(data.head())
-    print(data.index)
-    print(data.columns)
     # Need to figure out, how to normalize the dataframe
     #result = data.reset_index(level=2,col_level=2,drop=True)
     #result = result[["Date", "Close"]]
